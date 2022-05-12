69d3526b389905a18373fa45983350342059e962
Johannes Weber <johannes.k.weber@web.de>
2022-05-12 09:13:50 +0200

    started header menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  images = ['01.jpg', '02.jpg', '03.jpg']
-  currentImage = 0;
-  showImage = true;
+export class AppComponent {
 
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-
-      setTimeout(() => {
-        this.showImage = true;
-      }, 10)
-    }, 8000)
-  }
 }
---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+  images = ['01.jpg', '02.jpg', '03.jpg'];
+  headlines = [
+    'Every destination makes a dream come true!',
+    'Whether you are downtown in New York City',
+    'Or enjoying the beach in French Polynesia'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10)
+    }, 8000)
+  }
+
+}
